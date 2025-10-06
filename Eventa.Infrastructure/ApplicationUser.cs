23e93527a875d4f2b173553d3c5c0761d1b46ff5
Nikita-Titarenko <nikitatitarenko81@gmail.com>
2025-10-06 14:11:20 +0300

    Add ApplicationUser

---
+++ Eventa.Infrastructure/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Eventa.Infrastructure
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(32, MinimumLength = 3)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
