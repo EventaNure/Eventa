ee259b5f44fb811c69b13080bfdf4f09fe00417f
Nikita-Titarenko <nikitatitarenko81@gmail.com>
2025-10-06 14:12:19 +0300

    Add InfrastructureServiceExtensions

---
+++ Eventa.Infrastructure/InfrastructureServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eventa.Infrastructure
+{
+    public static class InfrastructureServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfigurationManager configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContextFactory<ApplicationDbContext>(options =>
+            {
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
