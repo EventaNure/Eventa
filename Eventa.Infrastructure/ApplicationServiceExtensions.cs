739a601d9ef95b749134b2b9a4390ecf2ccc79ce
Nikita-Titarenko <nikitatitarenko81@gmail.com>
2025-10-08 12:21:43 +0300

    Add ApplicationServiceExtensions

---
+++ Eventa.Infrastructure/ApplicationServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Eventa.Application.Services;
+using Eventa.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eventa.Infrastructure
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddAutoMapper((e) => { }, AppDomain.CurrentDomain.GetAssemblies());
+            return services;
+        }
+    }
+}
